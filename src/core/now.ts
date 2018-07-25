fe29babb398a0c31e8250e689b214105b4ad794b
Chris Pearce <hello@chrispearce.me>
2018-07-25 20:27:59 +0100

    ensure bindings to correct contexts

--- src/core/now.ts
+++ src/core/now.ts
@@ -4,7 +4,7 @@
  */
 export const now =
   typeof window !== 'undefined' && window.performance && window.performance.now
-    ? window.performance.now
+    ? window.performance.now.bind(window.performance)
     : () => Date.now();
 
 export default now;
--- src/core/requestAnimationFrame.ts
+++ src/core/requestAnimationFrame.ts
@@ -5,14 +5,15 @@ const FRAME_RATE = 1000 / 60;
 
 /** Provide fallback to native requestAnimationFrame locked at optimal frame rate */
 export function fallbackRequestAnimationFrame(
-  callback: (timestamp: number) => void
+  callback: (timestamp: number) => void,
 ) {
   return setTimeout(() => callback(now()), FRAME_RATE);
 }
 
 /** request a callback on the next available frame */
 export const requestAnimationFrame =
-  (typeof window !== 'undefined' && window.requestAnimationFrame) ||
+  (typeof window !== 'undefined' &&
+    window.requestAnimationFrame.bind(window)) ||
   fallbackRequestAnimationFrame;
 
 export default requestAnimationFrame;
